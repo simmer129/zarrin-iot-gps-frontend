1694301935438638b58e4b6e7edea30fd155e13d
simmer129 <simmer641@gmail.com>
2022-09-05 15:54:51 +0430

    edit map and marker

--- src/app/pages/dashboard/map/marker.service.ts
+++ src/app/pages/dashboard/map/marker.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';   
+import "leaflet-sidebar-v2";
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
+
   makeCapitalCircleMarkers(map: any) {
     throw new Error('Method not implemented.');
   }
@@ -13,17 +17,20 @@ export class MarkerService {
 
   constructor(private http: HttpClient) { }
 
+  
+
   makeCapitalMarkers(map: L.Map): void { 
     this.http.get(this.capitals).subscribe((res: any) => {
         for (const c of res.features) {
           const lon = c.geometry.coordinates[0];
           const lat = c.geometry.coordinates[1];
           const circle = L.circleMarker([lat, lon]);
-          //start sidebar
-
-          
+          //start sidebar       
+      
           //end sidebar
-          circle.addTo(map).on('click',()=>{console.log("hiiiiiii");});
+          circle.addTo(map).on('click',()=>{
+            console.log("hiiiii");
+          });
         }
       });
   }
